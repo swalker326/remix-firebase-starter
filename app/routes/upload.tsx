56b178731266da77c0f522c76be279eaab653d3c
Shane <shane.walker@facet.com.au>
2022-06-12 13:27:43 -0400

    added error handeling ish

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,4 +1,5 @@
-import { ActionFunction, json, LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, ErrorBoundaryComponent } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import {
   unstable_composeUploadHandlers,
   unstable_createFileUploadHandler,
@@ -21,7 +22,8 @@ export const action: ActionFunction = async ({ request }) => {
     uploadHandler
   );
 
-  const bucket = storage.bucket(process.env.storageBucket);
+  // const bucket = storage.bucket(process.env.storageBucket);
+  const bucket = storage.bucket("remix-starter.appspot.com");
   let fileUrl = {
     imageUrl: "",
     errors: "no image uploaded",
@@ -41,7 +43,7 @@ export const action: ActionFunction = async ({ request }) => {
         reject(`Error: ${error}`);
       })
       .end(fileBuffer, async () => {
-        await blob.makePublic();
+        // await blob.makePublic();
         resolve(fileUrl);
         fileUrl = {
           imageUrl: blob.publicUrl(),
@@ -59,14 +61,45 @@ export default function () {
 
   return (
     <div>
-      Upload a file to a firebase bucket
+      <h3>Upload a file to a firebase bucket</h3>
       {!!imageUrl && (
         <img style={{ maxWidth: "500px" }} src={imageUrl} alt="uploaded" />
       )}
       <Form method="post" encType="multipart/form-data">
-        <input type="file" name="my-file" />
-        <button type="submit">Upload</button>
+        <div
+          style={{
+            maxWidth: "800px",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <input
+            style={{ marginBottom: "1.5rem" }}
+            type="file"
+            name="my-file"
+          />
+          <button type="submit">Upload</button>
+        </div>
       </Form>
     </div>
   );
 }
+
+export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
+  console.error(error);
+  //Not sure about this, think I need to throw an error to trigger this
+  return (
+    <div>
+      <div style={{ marginBottom: "1.5rem" }}>
+        <h3>There was an error uploading your image, lets try that again</h3>
+        <h6>{error.message}</h6>
+      </div>
+      <Form method="post" encType="multipart/form-data">
+        <input type="file" name="my-file" />
+        <button type="submit">Upload</button>
+      </Form>
+      {/* add the UI you want your users to see */}
+      {/* <Scripts /> */}
+    </div>
+  );
+};
